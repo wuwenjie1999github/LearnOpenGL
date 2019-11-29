e335f6f264b9ad4a8d70a19c3a5bc88a4a9893f8
wuwenjie1999github <2580582774@qq.com>
2019-11-29 14:22:50 +0800

    修复鼠标观察BUG

--- Camera.cpp
+++ Camera.cpp
@@ -1,6 +1,15 @@
 #include "header/Angel.h"
 #include "header/Camera.h"
 
+
+Camera::Camera()
+{
+	camera_front.x = sin(DegreesToRadians * yaw) * cos(DegreesToRadians * pitch);
+	camera_front.y = -sin(DegreesToRadians * pitch);
+	camera_front.z = -cos(DegreesToRadians * yaw) * cos(DegreesToRadians * pitch);
+	camera_front = normalize(camera_front);
+}
+
 void Camera::setProgram(GLuint program)
 {
 	this->program = program;
--- header/Camera.h
+++ header/Camera.h
@@ -9,15 +9,15 @@ public:
 	GLuint program;
 
 	vec3 camera_pos = vec3(-5.0, 5.0, 5.0);
-	vec3 camera_front = vec3(1.0, -1.0, -1.0);
+	vec3 camera_front = vec3(0.0, 0.0, -1.0);
 	vec3 camera_up = vec3(0.0, 1.0, 0.0);
 
 	float deltaTime = 0.0f;	// 当前帧与上一帧的时间差
 	float lastFrame = 0.0f;	// 上一帧的时间
 
-	float yaw = 0.0f;
-	float pitch = 0.0f;
+	float yaw = 45.0f;
+	float pitch = 38.0f;
 
-	Camera() {}
+	Camera();
 	void setProgram(GLuint program);
 };
